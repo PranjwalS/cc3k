28f00020f33f11bc2fabe57dc0bf96bb7d238400
n2mir <n2mir@uwaterloo.ca>
2026-07-14 15:06:21 -0400

    Random module tweaks

--- src/random-impl.cc
+++ src/random-impl.cc
@@ -2,6 +2,7 @@ module random;
 
 import <random>;
 
+// Seed Mersenne Twister algorithm to generate gen
 static std::random_device seed;
 static std::mt19937 gen(seed());
 
@@ -15,14 +16,16 @@ bool randomChance(double probability) {
     return dist(gen) == 1;
 }
 
-int randomWeightedIndex(const double probabilities[], const int size) {
-    // Normalize probabilities
-    double total = 0.0;
-    for (int i = 0; i < size; ++i) {
-        total += probabilities[i];
+int randomWeightedIndex(const double probabilities[], const int size, const bool normalize) {
+    double total = 1.0;
+    // Normalize probabilities 
+    if (normalize) {
+        total = 0.0;
+        for (int i = 0; i < size; ++i) {
+            total += probabilities[i];
+        }
     }
     std::uniform_real_distribution<double> dist(0.0, total);
-
     double r = dist(gen);
 
     double sum = 0.0;
--- src/random.cc
+++ src/random.cc
@@ -1,7 +1,11 @@
 export module random;
 
+// Random number generator from min to max inclusive
 export int randomNum(const int min, const int max);
+
+// Return random bool given probability that true is chosen
 export bool randomChance(double probability);
 
 // Return random index of an array where probabilities[i] is the probability index i is chosen
-export int randomWeightedIndex(const double probabilities[], const int size);
+// If normalize enabled, normalize probabilities to standard uniform distribution
+export int randomWeightedIndex(const double probabilities[], const int size, const bool normalize = false);
