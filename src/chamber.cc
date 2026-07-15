9fcff3b7c3127636ad9d5617fb285c22e05ad7bd
n2mir <n2mir@uwaterloo.ca>
2026-07-15 14:39:50 -0400

    missed a spot

--- src/chamber.cc
+++ src/chamber.cc
@@ -18,8 +18,6 @@ export class Chamber {
     const std::vector<std::pair<int, int>>& getOccupiedCells() const { return occupiedCells; };
     const std::set<std::pair<int, int>>& getEmptyCells() const { return emptyCells; };
 
-    // void refreshEmptyCells();
-    // void addSpawnableCells(std::span<const std::pair<int, int>> cells);
     void removeEmpty(int x, int y);
     bool contains(int x, int y) const;
     std::optional<std::pair<std::pair<int, int>, constants::Direction>> randomHoard() const; // for dragon
